51ed8f581bd9323ea1b9d09906bc30080c912dc4
PhungGiaKiet <91674344+PhungGiaKiet@users.noreply.github.com>
2021-09-30 19:58:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Hmm...",
-    introTitle: 'Aluuu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    titleWeb: "Thế giới của Kietttt <3",
+    introTitle: 'Hmmmm...',
+    introDesc: `Vô tình gặp gỡ, thế là yêu :3`,
+    btnIntro: 'Tiếp tục...',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên?',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Mình biết mà kkk. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Mà yêu rồi thì nhắn tin đi chớ :3 Còn chần chờ gì nữa kk',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
